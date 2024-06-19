4af62c4ce2a8cc299dab8f11b0ca0aaf3c0ed1f1
FP2402386 <FP2402386@PCF77CD3386E90>
2024-06-19 14:20:52 +0200

    Projektdateien hinzufügen.

---
+++ Step2coder-Session10.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Step2coder_Session10._1
+{
+    internal class Program
+    {
+        private static Mutex m1 = new Mutex();
+
+        public static void donothingspecial()
+        {
+            // simulieren von MUTEX absichern dass man auf eine datei nicht schreibt und liest gleichzeitig..
+            //loese mittels mutex und C#
+            //ueberblick ueber die  methoden in mutex..
+
+            m1.WaitOne(1000); // nur auf eine ressource zugreifen..
+            //
+
+            // hier kommt rein was die funktion macht 
+
+            m1.ReleaseMutex();
+
+
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(donothingspecial));
+            Thread t2 = new Thread(new ThreadStart(donothingspecial));
+
+        }
+    }
+}
